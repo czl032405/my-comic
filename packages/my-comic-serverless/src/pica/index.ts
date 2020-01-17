52cfa64afc2fd7c5f701482d9684f7c220828ab9
czl <471060995@qq.com>
2020-01-17 11:26:33 +0800

    fix: allcomics

--- packages/my-comic-serverless/src/pica/index.ts
+++ packages/my-comic-serverless/src/pica/index.ts
@@ -1,8 +1,12 @@
 import Axios from "axios";
 export async function main(event, context) {
-  const { method, params } = event;
+  const { method = "", params = {} } = event;
   const BASE_URL = `https://my-comic.herokuapp.com/pica/`;
   let url = `${BASE_URL}${method}`;
+
+  if (method == "allcomics") {
+    params.maxPage = 1;
+  }
 
   let result = await Axios({
     method: "GET",
